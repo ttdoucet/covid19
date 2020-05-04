cf1373dccf11e26ce6f6b7f137d807c74a4d3901
Todd Doucet <todd@lambentresearch.com>
2020-05-03 20:47:19 -0400

    Added permyriad to county plots.

--- county.py
+++ county.py
@@ -20,31 +20,52 @@ combined_df['date'] = 'date'
 fips_df = combined_df[combined_df.FIPS.notnull()]
 fips_df['date'] = pd.to_datetime(fips_df['Last_Update'])
 
+def funcs(fips):
+    fips = int(fips)
+    def fwd(x):
+        return x / pops.county_pop(fips) * 10000
+
+    def rev(f):
+        return f * pops.county_pop(fips) / 10000
+
+    return (fwd, rev)
+
 def plot_them(fips):
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+    fips = str(fips)
     sdd = fips_df[fips_df.FIPS==int(fips)]
     sdd_sort = sdd.sort_values(by="date")
 
     def decorate(axis):
-        ax.set_xlabel('')
-        ax.get_legend().remove()
+        axis.set_xlabel('')
+        sec = axis.secondary_yaxis('right', functions=funcs(fips))
+        sec.set_ylabel('per 10k population')
+        axis.get_legend().remove()
 
-    place = sdd['Combined_Key'].iloc[0]
-    print("place: ", place)
+    if len(sdd.index) == 0:
+        print("no data")
+        return
 
+    place = pops.county_name(fips)
+    print(place + ": ", pops.county_pop(fips))
+
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
     ax = sdd.plot(ax=ax1, x='date', y='Deaths', logy=False, grid=True, title = "Deaths: " + place)
-    decorate(ax2)
-
-#    ax = sdd.plot(ax=ax1, x='date', y='Confirmed', logy=False, grid=True, title = "Confirmed: " + place)
-#    decorate(ax1)
+    decorate(ax)
 
     ax = sdd.plot(ax=ax2, x='date', y='Confirmed', logy=False, grid=True, title = "Confirmed: " + place)
-    decorate(ax1)
+    decorate(ax)
 
     fig.tight_layout()
 
+
+def fip_stat(fips):
+    pop = pops.county_pop(fips)
+    name = pops.county_name(fips)
+    print(name + ": " + str(pop) )
+
 if len(sys.argv) == 1:
-    plot_them("42003")
+    plot_them(42003) # Allegheny County
+
 else:
     for fips in sys.argv[1:]:
         plot_them(fips.upper())
---
+++ population.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import sys
+
+# Crufty but useful for debugging.
+from pandasgui import show
+
+counties = "county_population.csv"
+county_df = pd.read_csv(counties, encoding='latin-1')
+
+def county_pop(fips):
+    fips = int(fips)
+    # It is hard to believe the Pandas sucks this much.  I probably
+    # do not know what I am doing.
+    return county_df[county_df.county_fips==fips].iloc[0]['pop2014']
+
+def county_name(fips):
+    fips = int(fips)
+    return county_df[county_df.county_fips==fips].iloc[0]['county_name']
+
+#
+# Try to get rid of this and find a dataset or an API.
+#
+population = {'CA': 39.512e6,
+              'TX': 28.995e6,
+              'FL': 21.447e6,
+              'NY': 19.453e6,
+              'PA': 12.802e6,
+              'IL': 12.672e6,
+              'OH': 11.689e6,              
+              'GA': 10.617e6,
+              'NC': 10.488e6,
+              'MI': 9.987e6,
+              'NJ': 8.882e6,
+              'VA': 8.536e6,
+              'WA': 7.615e6,
+              'AZ': 7.279e6,
+              'MA': 6.950e6,
+              'TN': 6.833e6,
+              'IN': 6.732e6,
+              'MO': 6.137e6,
+              'MD': 6.046e6,
+              'WI': 5.822e6,
+              'CO': 5.759e6,
+              'MN': 5.640e6,
+              'SC': 5.149e6,
+              'AL': 4.903e6,
+              'LA': 4.649e6,
+              'KY': 4.468e6,
+              'OR': 4.218e6,
+              'OK': 3.957e6,
+              'CT': 3.565e6,
+              'UT': 3.206e6,
+              'IA': 3.155e6,
+              'NV': 3.080e6,
+              'AR': 3.018e6,
+              'MS': 2.976e6,
+              'KS': 2.913e6,
+              'NM': 2.097e6,
+              'NE': 1.934e6,
+              'ID': 1.787e6,
+              'WV': 1.792e6,
+              'HI': 1.415e6,
+              'NH': 1.360e6,
+              'ME': 1.344e6,
+              'MT': 1.069e6,
+              'RI': 1.059e6,
+              'DE': 973764,
+              'SD': 884659,
+              'ND': 762062,
+              'AK': 731545,
+              'DC': 705749,
+              'VT': 623989,
+              'WY': 578759
+             }
+
+def us_pop(state):
+    return population[state];
