1b905103c6ca61622e6107879d67071bac25d663
Todd Doucet <todd@lambentresearch.com>
2020-07-12 08:35:27 -0400

    State plots are either all cumulative or all daily.

--- state.py
+++ state.py
@@ -5,12 +5,13 @@ from matplotlib.dates import DateFormatter
 from scipy.signal import savgol_filter
 import numpy as np
 import sys
+import click
 
 import population as pops
 
 url = 'https://covidtracking.com/api/v1/states/daily.csv'
 dd = pd.read_csv(url,
-                 usecols=['date', 'state', 'positive', 'positiveIncrease', 'death'],
+                 usecols=['date', 'state', 'positive', 'positiveIncrease', 'death', 'deathIncrease'],
                  parse_dates=['date'],
                  index_col=['state']
                  )
@@ -29,8 +30,8 @@ def smooth(y):
     yhat = savgol_filter(y, 7, 1)
     return yhat
 
-def plot_them(state):
-    fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(10, 5))
+def plot_them(state, daily):
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
     sdd = dd.loc[state]
 
     def decorate(axis):
@@ -42,33 +43,57 @@ def plot_them(state):
         date_form = DateFormatter("%m-%d")
         axis.xaxis.set_major_formatter(date_form)
 
-    ax = sdd.plot(x='date', y='death', ax=ax1, grid=True, title=state+" deaths")
-    decorate(ax)
+    if daily == False:
+        ax = sdd.plot(x='date', y='death', ax=ax1, grid=True, title=state+" deaths")
+        decorate(ax)
+    else:
+        plot_color='#1f77b4'
+        ax = sdd.plot(ax=ax1, x='date', y='deathIncrease',
+                      color=plot_color, alpha=0.25,
+                      grid=True,
+                      title=state+ " daily deaths")
 
-    ax = sdd.plot(ax=ax2, x='date', y='positive', grid=True, title=state+ " cases")
-    decorate(ax)
+        delta = sdd.deathIncrease.values
+        delta[-1] = 0
 
-    plot_color='#1f77b4'
-    ax = sdd.plot(ax=ax3, x='date', y='positiveIncrease',
-                  color=plot_color, alpha=0.25,
-                  grid=True,
-                  title=state+ " daily cases")
+        plot_color='#1f77b4'
+        sdd['deathIncrease-smoothed'] = smooth(delta)
+        sdd.plot(ax=ax1, x='date',  y='deathIncrease-smoothed', grid=True, color=plot_color)
+        decorate(ax)
 
-    delta = sdd.positiveIncrease.values
-    delta[-1] = 0
+        pass
 
-    sdd['daily-cases-smoothed'] = smooth(delta)
-    sdd.plot(ax=ax3, x='date',  y='daily-cases-smoothed', grid=True, color=plot_color)
 
-    decorate(ax)
+    if daily == False:
+        ax = sdd.plot(ax=ax2, x='date', y='positive', grid=True, title=state+ " cases")
+        decorate(ax)
+    else:
+        plot_color='#1f77b4'
+        ax = sdd.plot(ax=ax2, x='date', y='positiveIncrease',
+                      color=plot_color, alpha=0.25,
+                      grid=True,
+                      title=state+ " daily cases")
+
+        delta = sdd.positiveIncrease.values
+        delta[-1] = 0
+
+        sdd['daily-cases-smoothed'] = smooth(delta)
+        sdd.plot(ax=ax2, x='date',  y='daily-cases-smoothed', grid=True, color=plot_color)
+        decorate(ax)
 
     fig.tight_layout()
-    # fig.savefig(state+".png")
 
-if len(sys.argv) == 1:
-    plot_them("PA")
-else:
-    for state in sys.argv[1:]:
-        plot_them(state.upper())
+@click.command()
+@click.option("--daily/--cumulative", default=True, help="Daily cases or total cases")
+@click.argument('states', nargs=-1)
+def cmdline(daily, states):
+    print("cmdline daily:", daily)
+    if len(states) == 0:
+        plot_them('PA', daily)
+    else:
+        for state in states:
+            plot_them(state.upper(), daily)
+    plt.show()
 
-plt.show()
+if __name__ == '__main__':
+    cmdline()
